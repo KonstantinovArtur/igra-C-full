0c890f36b5e5b1eeaf24d35740206896c7d7c284
KonstantinovArtur <144149895+KonstantinovArtur@users.noreply.github.com>
2023-10-16 15:06:23 +0300

    Поменял программу, добавил методы

--- Program.cs
+++ Program.cs
@@ -1,5 +1,65 @@
 ﻿int sifra;
 int a;
+
+
+static void rnd()
+{
+    int a;
+    Random rnd = new Random();
+    int value = rnd.Next(0, 100);
+    Console.WriteLine("Угадай число от 0 до 100");
+    Console.WriteLine("Введите число");
+    a = Convert.ToInt32(Console.ReadLine());
+    while (a != value)
+    {
+        if (a < value)
+        {
+            Console.WriteLine("Больше");
+            Console.WriteLine("Введите число");
+            a = Convert.ToInt32(Console.ReadLine());
+        }
+        else if (a > value)
+        {
+            Console.WriteLine("Меньше");
+            Console.WriteLine("Введите число");
+            a = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    Console.WriteLine("Угадал");
+}
+static void umn()
+{
+    int[,] myltiplytable = new int[10, 10];
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
+            myltiplytable[i, j] = i * j;
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine();
+        for (int j = 1; j < 10; j++)
+            Console.Write("{0, 3}", myltiplytable[i, j]);
+        Console.WriteLine();
+    }
+}
+static void del()
+{
+    Console.WriteLine("Введите число");
+    string b = Console.ReadLine();
+    int c = Convert.ToInt32(b);
+    int d = 0;
+    while (d <= c)
+    {
+        d++;
+        if (c % d == 0)
+        {
+            Console.WriteLine(d);
+        }
+        else if (c % d != 0)
+        {
+            continue;
+        }
+    }
+}
 do
 {
     Console.WriteLine("Какую программу вы хотите запустить?");
@@ -10,61 +70,15 @@ do
     sifra = Convert.ToInt32(Console.ReadLine());
     if (sifra == 1)
     {
-        Random rnd = new Random();
-        int value = rnd.Next(0, 100);
-        Console.WriteLine("Угадай число от 0 до 100");
-        Console.WriteLine("Введите число");
-        a = Convert.ToInt32(Console.ReadLine());
-        while (a != value)
-        {
-            if (a < value)
-            {
-                Console.WriteLine("Больше");
-                Console.WriteLine("Введите число");
-                a = Convert.ToInt32(Console.ReadLine());
-            }
-            else if (a > value)
-            {
-                Console.WriteLine("Меньше");
-                Console.WriteLine("Введите число");
-                a = Convert.ToInt32(Console.ReadLine());
-            }
-        }
-        Console.WriteLine("Угадал");
+        rnd();
     }
     else if (sifra == 2)
     {
-        int[,] myltiplytable = new int[10, 10];
-        for (int i = 0; i < 10; i++)
-            for (int j = 0; j < 10; j++)
-                myltiplytable[i, j] = i * j;
-        for (int i = 0; i < 10; i++)
-        {
-            Console.WriteLine();
-            for (int j = 1; j < 10; j++)
-                Console.Write("{0, 3}", myltiplytable[i, j]);
-            Console.WriteLine();
-        }
+        umn();
     }
     else if (sifra == 3)
     {
-        Console.WriteLine("Введите число");
-        string b = Console.ReadLine();
-        int c = Convert.ToInt32(b);
-        int d = 0;
-        while (d <= c)
-        {
-            d++;
-            if (c % d == 0)
-            {
-                Console.WriteLine(d);
-            }
-            else if (c % d != 0)
-            {
-                continue;
-            }
-        }
-
+        del();
     }
 } while (sifra != 4);
 
